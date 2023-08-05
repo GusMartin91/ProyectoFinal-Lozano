15670dbf2b1fb0fd7b91c89f3d4b98bc5c7f8759
GusMartin91 <gustavomlozano@gmail.com>
2023-08-05 00:08:18 -0300

    Subiendo Basicas

---
+++ assets/js/funciones.js
@@ -0,0 +1,51 @@
+function nuevaCompra() {
+    let dinero = Number(prompt("¿Cuanto dinero desea ingresar a la Maquina Expendedora?"))
+    let producto = prompt("Elija uno de los productos disponibles: Gaseosa, Alfajor, Chicles, Papas Fritas, Galletitas Dulces")
+    alert(maquinaExpendedora(dinero, producto))
+}
+function maquinaExpendedora(dinero, producto) {
+    switch (producto) {
+        case "Gaseosa":
+            if (dinero > 380) {
+                let vuelto = dinero - 380
+                return "Aqui tienes tu " + producto + " fresquita, y este es tu vuelto: $" + vuelto + ".-"
+            } else {
+                let restanIngresar = 380 - dinero
+                return "Dinero insuficiente, te falta agregar $" + restanIngresar + ".-"
+            }
+        case "Alfajor":
+            if (dinero > 220) {
+                let vuelto = dinero - 220
+                return "Aqui tienes tu " + producto + ", y este es tu vuelto: $" + vuelto + ".-"
+            } else {
+                let restanIngresar = 220 - dinero
+                return "Dinero insuficiente, te falta agregar $" + restanIngresar + ".-"
+            }
+        case "Chicles":
+            if (dinero > 185) {
+                let vuelto = dinero - 185
+                return "Aqui tienes tus " + producto + ", y este es tu vuelto: $" + vuelto + ".-"
+            } else {
+                let restanIngresar = 185 - dinero
+                return "Dinero insuficiente, te falta agregar $" + restanIngresar + ".-"
+            }
+        case "Papas Fritas":
+            if (dinero > 478) {
+                let vuelto = dinero - 478
+                return "Aqui tienes tus " + producto + " crocantes, y este es tu vuelto: $" + vuelto + ".-"
+            } else {
+                let restanIngresar = 478 - dinero
+                return "Dinero insuficiente, te falta agregar $" + restanIngresar + ".-"
+            }
+        case "Galletitas Dulces":
+            if (dinero > 328) {
+                let vuelto = dinero - 328
+                return "Aqui tienes tus " + producto + ", y este es tu vuelto: $" + vuelto + ".-"
+            } else {
+                let restanIngresar = 328 - dinero
+                return "Dinero insuficiente, te falta agregar $" + restanIngresar + ".-"
+            }
+        default:
+            return "No ingresaste un producto valido o nos quedamos sin stock, vuelve a intentar"
+    }
+}
